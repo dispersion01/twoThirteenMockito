ea8305fe4c808273b0ceeeeb274387e7a16d57a8
dispersion <anyta1901@gmail.com>
2022-03-31 12:26:23 +0300

    Initial commit

---
+++ src/main/java/com/any/twothirteenmockito/TwoThirteenMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.any.twothirteenmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoThirteenMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwoThirteenMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/any/twothirteenmockito/TwoThirteenMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.any.twothirteenmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoThirteenMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
